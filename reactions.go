37344bfcc43703b2bfa27341ac45f6f9ea627155
Ben Guidarelli <ben.guidarelli@gmail.com>
2021-11-15 07:24:45 -0500

    fix emojis

--- reactions.go
+++ reactions.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/google/go-github/v40/github" // with go modules enabled (GO111MODULE=on or outside GOPATH)
 )
@@ -24,17 +25,23 @@ func main() {
 		log.Fatalf("Failed to get issues: %+v", err)
 	}
 
-	fmt.Printf("%s", template)
-	for _, issue := range issues {
-		fmt.Printf("- *%s* ", *issue.Title)
-
-		fmt.Printf(" :plusone: %d", *issue.Reactions.PlusOne)
-		fmt.Printf(" :minusone: %d", *issue.Reactions.MinusOne)
-		fmt.Printf(" :laugh: %d", *issue.Reactions.Laugh)
-		fmt.Printf(" :confused: %d", *issue.Reactions.Confused)
-		fmt.Printf(" :heart: %d", *issue.Reactions.Heart)
-		fmt.Printf(" :hooray: %d", *issue.Reactions.Hooray)
-		fmt.Printf(" :rocket: %d", *issue.Reactions.Rocket)
-		fmt.Printf(" :eyes: %d\n", *issue.Reactions.Eyes)
+	f, err := os.Create("REACTIONS.md")
+	if err != nil {
+		log.Fatalf("Failed to create README file: %+v", err)
 	}
+
+	fmt.Fprintf(f, "%s", template)
+	for _, issue := range issues {
+		fmt.Fprintf(f, "- *%s* ", *issue.Title)
+		fmt.Fprintf(f, " :heavy_plus_sign: %d", *issue.Reactions.PlusOne)
+		fmt.Fprintf(f, " :heavy_minus_sign: %d", *issue.Reactions.MinusOne)
+		fmt.Fprintf(f, " :laughing: %d", *issue.Reactions.Laugh)
+		fmt.Fprintf(f, " :confused: %d", *issue.Reactions.Confused)
+		fmt.Fprintf(f, " :heart: %d", *issue.Reactions.Heart)
+		fmt.Fprintf(f, " :tada: %d", *issue.Reactions.Hooray)
+		fmt.Fprintf(f, " :rocket: %d", *issue.Reactions.Rocket)
+		fmt.Fprintf(f, " :eyes: %d\n", *issue.Reactions.Eyes)
+	}
+
+	f.Close()
 }
